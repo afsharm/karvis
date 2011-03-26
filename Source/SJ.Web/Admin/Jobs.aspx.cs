3bfe574bcb7641cd5ced5723506228223f6159dc
afsharm <afshar.mohebbi@gmail.com>
2011-03-26 11:45:42 +0430

    add web project

---
+++ Source/SJ.Web/Admin/Jobs.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SJ.Web.Admin
+{
+    public partial class Jobs : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
