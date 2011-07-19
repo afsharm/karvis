27e264d5df2356df6709e640c2bcb874ec5fbd04
mohebbi <afshar.mohebbi@gmail.com>
2011-07-19 11:12:55 +0430

    initial step for adding crawler

---
+++ Source/SJ.Core/Crawler/Crawler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace SJ.Core
+{
+    public class Crawler
+    {
+        public string GetWebText(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+
+            request.UserAgent = "Karvis Web Crawler";
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string htmlText = reader.ReadToEnd();
+
+            return htmlText;
+        }
+    }
+}
