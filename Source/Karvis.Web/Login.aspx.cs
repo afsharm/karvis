016962826adf32bafe2def12942ee2898018aa27
afsharm (l) <afshar.mohebbi@gmail.com>
2011-09-05 23:43:21 +0430

    getting ready to use sqlite asp.net membership provider (http://www.eggheadcafe.com/articles/20051119.asp)

---
+++ Source/Karvis.Web/Login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Karvis.Web
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CreateUserWizard1_CreatedUser(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
