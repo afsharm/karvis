8af4449b04aa71884cba5e1dea8145a4ca8e41e6
afsharm (l) <afshar.mohebbi@gmail.com>
2011-10-19 15:18:46 +0330

    KMail simple functionality has been added

---
+++ Source/Karvis.Test/KMailModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Karvis.Core;
+using NUnit.Framework;
+
+namespace Karvis.Test
+{
+
+    [TestFixture]
+    public class KMailModelTest : NHibernateFixture
+    {
+
+
+        [Test]
+        public void SimpleTest()
+        {
+            IKMailModel model = new KMailModel(SessionFactory);
+
+            string subject = Guid.NewGuid().ToString();
+            string description = Guid.NewGuid().ToString();
+            string fromAddress = Guid.NewGuid().ToString();
+            string formDescription = Guid.NewGuid().ToString();
+            int relatedReferenceId = new Random(DateTime.Now.Millisecond).Next(1, 10000);
+
+            model.Qeue(subject, description, fromAddress, formDescription, relatedReferenceId);
+
+            var sent = model.GetSentItems();
+            var unsent = model.GetUnSentItems();
+
+            Assert.AreEqual(0, sent.Count);
+            Assert.AreEqual(1, unsent.Count);
+
+        }
+    }
+}
+
