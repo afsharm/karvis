4dd65e2ca67dd19878903ec81877299d89a7a40e
shahin kiassat <shaahin69@gmail.com>
2012-08-15 00:30:58 +0430

    update DI settings

--- Source/MVC/app/Karvis.Init/DependencyResolverInitializer.cs
+++ Source/MVC/app/Karvis.Init/DependencyResolverInitializer.cs
@@ -1,6 +1,11 @@
 ﻿using System.Web.Mvc;
 using Karvis.NHibernateProvider;
 using NHibernate;
+using NHibernate.Cfg;
+using Razmyar.Domain.Contracts.Repositories;
+using Razmyar.Domain.Contracts.Tasks;
+using Razmyar.SharpLite.Repositories;
+using Razmyar.SharpLite.Tasks;
 using SharpLite.Domain.DataInterfaces;
 using SharpLite.NHibernateProvider;
 using StructureMap;
@@ -9,16 +14,29 @@ namespace Karvis.Init
 {
     public class DependencyResolverInitializer
     {
-        public static void Initialize() {
-            var container = new Container(x => {
-                x.For<ISessionFactory>()
-                    .Singleton()
-                    .Use(() => NHibernateInitializer.Initialize().BuildSessionFactory());
-                x.For<IEntityDuplicateChecker>().Use<EntityDuplicateChecker>();
-                x.For(typeof(IRepository<>)).Use(typeof(Repository<>));
-                x.For(typeof(IRepositoryWithTypedId<,>)).Use(typeof(RepositoryWithTypedId<,>));
-            });
+        public static void Initialize()
+        {
+            var container = new Container(x =>
+                                              {
+                                                  x.For<ISessionFactory>()
+                                                      .Singleton()
+                                                      .Use(
+                                                          () => NHibernateInitializer.Initialize().BuildSessionFactory());
+                                                  x.For<Configuration>() //needed for Settings/ConfigDb
+                                                      .Singleton()
+                                                      .Use(
+                                                          NHibernateInitializer.Initialize);
+                                                  x.For<IEntityDuplicateChecker>().Use<EntityDuplicateChecker>();
+                                                  x.For(typeof (IRepository<>)).Use(typeof (Repository<>));
+                                                  x.For(typeof (ICrudTask<>)).Use(typeof (CrudTask<>));
+                                                  x.For<IUserRepository>().Use<UserRepository>();
+                                                  x.For<IUserTask>().Use<UserTask>();
+                                                  x.For<IConfigDbTask>().Use<ConfigDbTask>();
+                                                  x.For(typeof (IRepositoryWithTypedId<,>)).Use(
+                                                      typeof (RepositoryWithTypedId<,>));
+                                              });
 
+            // ServiceLocator.SetLocatorProvider(() => new StructureMapServiceLocator(container));
             DependencyResolver.SetResolver(new StructureMapDependencyResolver(container));
         }
     }
