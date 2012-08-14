5bddb978c4d8c643362f5669854b11a736adbffa
shahin kiassat <shaahin69@gmail.com>
2012-08-14 23:10:41 +0430

    add config controller

---
+++ Source/MVC/app/Karvis.Web/Areas/Admin/Controllers/ConfigController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Karvis.Web.Areas.Admin.Controllers
+{
+    public class ConfigController : Controller
+    {
+        //
+        // GET: /Admin/Config/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
