7354f8d92136d1fb6bb7c7f79f7e8dec51702651
shahin kiassat <shaahin69@gmail.com>
2012-08-14 21:45:15 +0430

    create karvis sharp lite project

--- Source/Karvis.Core/Feed/IFeedExtractor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-namespace Karvis.Core
-{
-    public interface IFeedExtractor
-    {
-        List<Job> ExtractFeed(AdSource siteSource);
-    }
-}
--- Source/Karvis.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Karvis.Web.Global" Language="C#" %>
