9f91f2c50c777f5b68ead5ff9d629e5fa748111e
shahin kiassat <shaahin69@gmail.com>
2012-08-15 01:07:10 +0430

    add job view model class

---
+++ Source/MVC/app/Karvis.Tasks/ViewModels/JobViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Karvis.Tasks.ViewModels
+{
+    public class JobViewModel
+    {
+        public string Id { get; set; }
+
+        [DisplayName("عنوان")]
+        public string Title { get; set; }
+
+        [DisplayName("تگ")]
+        public string Tag { get; set; }
+
+        [DisplayName("تاریخ ثبت")]
+        public string RegistredDate { get; set; }
+
+        [DisplayName("تعداد مشاهده")]
+        public string VisitsCount { get; set; }
+
+        [DisplayName("منبع")]
+        public string Source { get; set; }
+    }
+}
