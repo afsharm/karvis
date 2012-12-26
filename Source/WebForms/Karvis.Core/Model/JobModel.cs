0bb36ab59878468fa7cef673be711e9ffd32ba15
shahin kiassat <shaahin69@gmail.com>
2012-12-27 03:19:42 +0330

    changed jobmodel

--- Source/WebForms/Karvis.Core/Model/JobModel.cs
+++ Source/WebForms/Karvis.Core/Model/JobModel.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Mail;
+using Fardis;
 using NHibernate;
 using NHibernate.Criterion;
-using Fardis;
-using NHibernate.Linq;
-using System.Net.Mail;
-using System.Net;
 
 namespace Karvis.Core
 {
     public class JobModel : IJobModel
     {
-        ISessionFactory _sessionFactory;
-        NHibernateRepository<Job> _jobRepository;
-        const int recentJobsCount = 10;
+        private const int recentJobsCount = 10;
+        private readonly NHibernateRepository<Job> _jobRepository;
+        private readonly ISessionFactory _sessionFactory;
 
         public JobModel()
         {
@@ -28,6 +26,8 @@ namespace Karvis.Core
             _sessionFactory = sessionFactory;
             _jobRepository = new NHibernateRepository<Job>(_sessionFactory);
         }
+
+        #region IJobModel Members
 
         public string GetJobUrl(Job job)
         {
@@ -58,17 +58,17 @@ namespace Karvis.Core
             if (!url.StartsWith("http://"))
                 url = "http://" + url;
 
-            Job job = new Job()
-            {
-                Title = title,
-                Description = description,
-                Url = url,
-                Tag = tag,
-                DateAdded = DateTime.UtcNow,
-                VisitCount = 0,
-                AdSource = adSource,
-                IsActive = true
-            };
+            var job = new Job
+                          {
+                              Title = title,
+                              Description = description,
+                              Url = url,
+                              Tag = tag,
+                              DateAdded = DateTime.UtcNow,
+                              VisitCount = 0,
+                              AdSource = adSource,
+                              IsActive = true
+                          };
 
             _jobRepository.Add(job);
 
@@ -81,27 +81,8 @@ namespace Karvis.Core
             return GeneralHelper.AnalyseTags(tags);
         }
 
-        private IQueryOver<Job, Job> CreateQuery(string title, string tag, AdSource adSource, bool? isActive)
-        {
-            var q = _jobRepository.QueryOver();
-
-            if (isActive != null)
-                q = q.Where(job => job.IsActive == isActive);
-
-            if (!string.IsNullOrEmpty(title))
-                q = q.WhereRestrictionOn(j => j.Title).IsInsensitiveLike(title, MatchMode.Anywhere);
-
-            if (!string.IsNullOrEmpty(tag))
-                q = q.WhereRestrictionOn(j => j.Tag).IsInsensitiveLike(tag, MatchMode.Anywhere);
-
-            if (adSource != AdSource.All)
-                q = q.Where(job => job.AdSource == adSource);
-
-            return q;
-        }
-
         public IList<Job> FindAll(string title, string tag, AdSource adSource, bool? isActive,
-            string sortOrder, int maximumRows, int startRowIndex)
+                                  string sortOrder, int maximumRows, int startRowIndex)
         {
             IQueryOver<Job, Job> q = CreateQuery(title, tag, adSource, isActive);
 
@@ -190,15 +171,6 @@ namespace Karvis.Core
             SendMail(job);
         }
 
-        void IncreaseVisitCount(int id)
-        {
-            Job job = _jobRepository.Load(id);
-
-            job.VisitCount++;
-
-            _jobRepository.SaveOrUpdate(job);
-        }
-
         public IEnumerable<Job> GetAllJobs()
         {
             return GetAllJobs(false);
@@ -206,13 +178,14 @@ namespace Karvis.Core
 
         public IEnumerable<Job> GetAllJobs(bool updateStat)
         {
-            var q = _jobRepository.QueryOver().Where(job => job.IsActive).OrderBy(j => j.DateAdded).Desc;
+            IQueryOver<Job, Job> q =
+                _jobRepository.QueryOver().Where(job => job.IsActive).OrderBy(j => j.DateAdded).Desc;
 
             if (updateStat)
             {
-                var recentJobs = q.Skip(0).Take(recentJobsCount).List<Job>();
+                IList<Job> recentJobs = q.Skip(0).Take(recentJobsCount).List<Job>();
 
-                foreach (var job in recentJobs)
+                foreach (Job job in recentJobs)
                 {
                     job.FeedCount++;
 
@@ -220,30 +193,14 @@ namespace Karvis.Core
                 }
             }
 
-            var jobs = q.Skip(0).Take(int.MaxValue).List<Job>();
+            IList<Job> jobs = q.Skip(0).Take(int.MaxValue).List<Job>();
             return jobs;
         }
 
         public IList<string> GetAllTags()
         {
-            var q = _jobRepository.QueryOver().Where(job => job.IsActive).Select(j => j.Tag);
+            IQueryOver<Job, Job> q = _jobRepository.QueryOver().Where(job => job.IsActive).Select(j => j.Tag);
             return q.List<String>();
-        }
-
-        internal IEnumerable<Job> GetJobsByTag(string tag, bool updateStat)
-        {
-            var jobs = FindAll(null, tag, AdSource.All, true, null, int.MaxValue, 0);
-
-            if (updateStat)
-            {
-                foreach (var job in jobs)
-                {
-                    job.FeedCount++;
-                    _jobRepository.SaveOrUpdate(job);
-                }
-            }
-
-            return jobs;
         }
 
         public Job GetJob(int jobId)
@@ -297,56 +254,10 @@ namespace Karvis.Core
             return jobs.Count;
         }
 
-        private void AddComplementaryInfo(Job job)
-        {
-            job.DateAdded = DateTime.UtcNow;
-            job.VisitCount = 0;
-            job.FeedCount = 0;
-        }
-
         public void SaveOrUpdateJob(Job job)
         {
             _jobRepository.SaveOrUpdate(job);
             SendMail(job);
-        }
-
-        private void SendMail(Job job)
-        {
-            //dont send email for none actis
-            if (!job.IsActive)
-                return;
-
-            string[] emails = job.Emails.Split(',');
-
-            //send for valid emails
-            if (emails.Length < 1)
-                return;
-
-            SmtpClient client = new SmtpClient("mail.karvis.ir", 25);
-            client.Credentials = new NetworkCredential("info@karvis.ir", "ra5c2y&f");
-
-            foreach (var email in emails)
-            {
-                if (string.IsNullOrEmpty(email))
-                    return;
-
-                string subject = "آگهی استخدام شما در «کارویس» به ثبت رسید";
-                
-                string body = string.Format(
-                    "عنوان: {0}\n\rشرح: {1}\n\rتگ: {2}\n\rتاریخ: {3}\n\rلینک: {4}\n\rکارویس http://karvis.ir سایت کاریابی مخصوص برنامه‌نویسان"
-                , job.Title, job.Description, job.Tag, job.DateAddedPersian, job.Url);
-
-                MailMessage message = new MailMessage(new MailAddress("no-reply@karvis.ir", "کارویس"), new MailAddress(email, email));
-                message.To.Add("afshar.mohebbi@gmail.com");
-                message.Body = body;
-                message.Subject = subject;
-
-                try
-                {
-                    client.Send(message);
-                }
-                catch { }
-            }
         }
 
         public void AddJob(Job job)
@@ -362,9 +273,11 @@ namespace Karvis.Core
 
         public string GetLastJobUrl(AdSource siteSource)
         {
-            var q = _jobRepository.QueryOver().Where(j => j.AdSource == siteSource && j.IsActive).OrderBy(j => j.DateAdded).Desc;
+            IQueryOver<Job, Job> q =
+                _jobRepository.QueryOver().Where(j => j.AdSource == siteSource && j.IsActive).OrderBy(j => j.DateAdded).
+                    Desc;
 
-            var res = q.Take(1).List();
+            IList<Job> res = q.Take(1).List();
 
             if (res != null && res.Count > 0)
                 return res[0].Url;
@@ -374,14 +287,15 @@ namespace Karvis.Core
 
         public bool ExistsJobUrl(string jobUrl)
         {
-            var q = _jobRepository.QueryOver().Where(j => j.Url.ToLower().Trim() == jobUrl.ToLower().Trim());
+            IQueryOver<Job, Job> q =
+                _jobRepository.QueryOver().Where(j => j.Url.ToLower().Trim() == jobUrl.ToLower().Trim());
 
             return q.RowCount() > 0;
         }
 
         public IList<string> GetJobUrlsByAdSource(AdSource siteSource)
         {
-            var q = _jobRepository.QueryOver().Where(j => j.AdSource == siteSource).Select(j => j.Url);
+            IQueryOver<Job, Job> q = _jobRepository.QueryOver().Where(j => j.AdSource == siteSource).Select(j => j.Url);
             return q.List<string>();
         }
 
@@ -390,9 +304,9 @@ namespace Karvis.Core
             IList<AdSourceStatDto> result = new List<AdSourceStatDto>();
             int totalCount = GetTotalJobCount();
 
-            foreach (var item in Enum.GetValues(typeof(AdSource)))
+            foreach (object item in Enum.GetValues(typeof (AdSource)))
             {
-                AdSource siteSource = (AdSource)item;
+                var siteSource = (AdSource) item;
 
                 if (siteSource == AdSource.All)
                     continue;
@@ -401,30 +315,125 @@ namespace Karvis.Core
                 if (count < 1)
                     continue;
 
-                int percent = (count * 100) / totalCount;
+                int percent = (count*100)/totalCount;
                 string siteSourceDescription = Job.GetAdSourceDescription(siteSource);
 
                 result.Add(new AdSourceStatDto
-                {
-                    SiteSource = (AdSource)item,
-                    SiteSourceDescription = siteSourceDescription,
-                    Count = count,
-                    Percent = percent
-                });
+                               {
+                                   SiteSource = (AdSource) item,
+                                   SiteSourceDescription = siteSourceDescription,
+                                   Count = count,
+                                   Percent = percent
+                               });
             }
 
-            var sorted = result.OrderByDescending(dto => dto.Count);
+            IOrderedEnumerable<AdSourceStatDto> sorted = result.OrderByDescending(dto => dto.Count);
             return sorted.ToList();
-        }
-
-        private int AdSourceCount(AdSource siteSource)
-        {
-            return _jobRepository.QueryOver().Where(j => j.IsActive && j.AdSource == siteSource).RowCount();
         }
 
         public int GetTotalJobCount()
         {
             return _jobRepository.QueryOver().Where(j => j.IsActive).RowCount();
+        }
+
+        #endregion
+
+        private IQueryOver<Job, Job> CreateQuery(string title, string tag, AdSource adSource, bool? isActive)
+        {
+            IQueryOver<Job, Job> q = _jobRepository.QueryOver();
+
+            if (isActive != null)
+                q = q.Where(job => job.IsActive == isActive);
+
+            if (!string.IsNullOrEmpty(title))
+                q = q.WhereRestrictionOn(j => j.Title).IsInsensitiveLike(title, MatchMode.Anywhere);
+
+            if (!string.IsNullOrEmpty(tag))
+                q = q.WhereRestrictionOn(j => j.Tag).IsInsensitiveLike(tag, MatchMode.Anywhere);
+
+            if (adSource != AdSource.All)
+                q = q.Where(job => job.AdSource == adSource);
+
+            return q;
+        }
+
+        private void IncreaseVisitCount(int id)
+        {
+            Job job = _jobRepository.Load(id);
+
+            job.VisitCount++;
+
+            _jobRepository.SaveOrUpdate(job);
+        }
+
+        internal IEnumerable<Job> GetJobsByTag(string tag, bool updateStat)
+        {
+            IList<Job> jobs = FindAll(null, tag, AdSource.All, true, null, int.MaxValue, 0);
+
+            if (updateStat)
+            {
+                foreach (Job job in jobs)
+                {
+                    job.FeedCount++;
+                    _jobRepository.SaveOrUpdate(job);
+                }
+            }
+
+            return jobs;
+        }
+
+        private void AddComplementaryInfo(Job job)
+        {
+            job.DateAdded = DateTime.UtcNow;
+            job.VisitCount = 0;
+            job.FeedCount = 0;
+        }
+
+        private void SendMail(Job job)
+        {
+            //dont send email for none actis
+            if (!job.IsActive)
+                return;
+
+            string[] emails = job.Emails.Split(',');
+
+            //send for valid emails
+            if (emails.Length < 1)
+                return;
+
+            var client = new SmtpClient("mail.karvis.ir", 25);
+            client.Credentials = new NetworkCredential("info@karvis.ir", "ra5c2y&f");
+
+            foreach (string email in emails)
+            {
+                if (string.IsNullOrEmpty(email))
+                    return;
+
+                string subject = "آگهی استخدام شما در «کارویس» به ثبت رسید";
+
+                string body = string.Format(
+                    "عنوان: {0}\n\rشرح: {1}\n\rتگ: {2}\n\rتاریخ: {3}\n\rلینک: {4}\n\rکارویس http://karvis.ir سایت کاریابی مخصوص برنامه‌نویسان"
+                    , job.Title, job.Description, job.Tag, job.DateAddedPersian, job.Url);
+
+                var message = new MailMessage(new MailAddress("no-reply@karvis.ir", "کارویس"),
+                                              new MailAddress(email, email));
+                message.To.Add("afshar.mohebbi@gmail.com");
+                message.Body = body;
+                message.Subject = subject;
+
+                try
+                {
+                    client.Send(message);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private int AdSourceCount(AdSource siteSource)
+        {
+            return _jobRepository.QueryOver().Where(j => j.IsActive && j.AdSource == siteSource).RowCount();
         }
     }
 }
